976675d4bbfd1fa6d9cd0e2bd51e37b505c8908a
s205057 <s205057@dtu.dk>
2024-04-15 22:23:13 +0200

    setup compatible with both linux and windows (det siger chatter drengen i hvert fald)

--- setup.sh
+++ setup.sh
@@ -1,10 +1,7 @@
-pip install --upgrade pip
+python -m pip install --upgrade pip
 
-pip install opencv-python==4.9.0.80
+python -m pip install opencv-python==4.9.0.80
 
-pip install imutils
+python -m pip install imutils
 
-pip install matplotlib==3.8.4
-
-
-echo "Installation completed successfully."
+python -m pip install matplotlib==3.8.4
