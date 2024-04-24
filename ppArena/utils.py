9e605f74551b0da03b92f42b895689c9613e5de8
Marta <118281459+BatWoman31@users.noreply.github.com>
2024-04-24 21:38:38 +0200

    godnat

--- ppArena/utils.py
+++ ppArena/utils.py
@@ -872,13 +872,17 @@ def CannyEdgeGray(image):
    # Display the cropped image
 #    cv2.imshow("cropped", cropped_image)
 
+
+
+
+
 def testcrosssearch(image):
     #https://pyimagesearch.com/2014/04/21/building-pokedex-python-finding-game-boy-screen-step-4-6/
     
     # image = cv2.imread(argparse["query"])
-    ratio = image.shape[0] / 500.0
+    ratio = image.shape[0] / 400
     orig = image.copy()
-    image = imutils.resize(image, height = 500)
+    image = imutils.resize(image, height = 400)
 
     # convert the image to grayscale, blur it, and find edges
     # in the image
@@ -908,6 +912,66 @@ def testcrosssearch(image):
          screenCnt = approx
          break
        
-    cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 3) 
+    cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2) 
     cv2.imshow("fingers crossed", image) 
-    cv2.waitKey(0)
+    # cv2.waitKey(0)
+
+    # pts = screenCnt.reshape(12, 2)
+    # rect = np.zeros((12, 2), dtype = "float32")
+
+    # # the top-left point has the smallest sum whereas the
+    # # bottom-right has the largest sum
+    # s = pts.sum(axis = 1)
+    # rect[0] = pts[np.argmin(s)]
+    # rect[2] = pts[np.argmax(s)]
+    # #   compute the difference between the points -- the top-right
+    # # will have the minumum difference and the bottom-left will
+    # # have the maximum difference
+    # diff = np.diff(pts, axis = 1)
+    # rect[1] = pts[np.argmin(diff)]
+    # rect[3] = pts[np.argmax(diff)]
+    # # multiply the rectangle by the original ratio
+    # rect *= ratio
+
+    # # now that we have our rectangle of points, let's compute
+    # # the width of our new image
+    # (tl, tr, br, bl) = rect
+    # widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    # widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    # # ...and now for the height of our new image
+    # heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    # heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    # # take the maximum of the width and height values to reach
+    # # our final dimensions
+    # maxWidth = max(int(widthA), int(widthB))
+    # maxHeight = max(int(heightA), int(heightB))
+    # # construct our destination points which will be used to
+    # # map the screen to a top-down, "birds eye" view
+    # dst = np.array([
+	# [0, 0],
+	# [maxWidth - 1, 0],
+	# [maxWidth - 1, maxHeight - 1],
+	# [0, maxHeight - 1]], dtype = "float32")
+    # # calculate the perspective transform matrix and warp
+    # # the perspective to grab the screen
+    # M = cv2.getPerspectiveTransform(rect, dst)
+    # warp = cv2.warpPerspective(orig, M, (maxWidth, maxHeight))
+
+    # # convert the warped image to grayscale and then adjust
+    # # the intensity of the pixels to have minimum and maximum
+    # # values of 0 and 255, respectively
+    # warp = cv2.cvtColor(warp, cv2.COLOR_BGR2GRAY)
+    # arp = exposure.rescale_intensity(warp, out_range = (0, 255))
+    # # the pokemon we want to identify will be in the top-right
+    # # corner of the warped image -- let's crop this region out
+    # (h, w) = warp.shape
+    # (dX, dY) = (int(w * 0.4), int(h * 0.45))
+    # crop = warp[10:dY, w - dX:w - 10]
+    # # save the cropped image to file
+    # cv2.imwrite("cropped.png", crop)
+    # # show our images
+    # cv2.imshow("image", image)
+    # cv2.imshow("edge", edged)
+    # cv2.imshow("warp", imutils.resize(warp, height = 400))
+    # cv2.imshow("crop", imutils.resize(crop, height = 400))
+    # cv2.waitKey(0)
