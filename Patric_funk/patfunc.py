6a83759545070bc65d171a86784710a3e286e01e
PatricTindo <patricseb@gmail.com>
2024-06-07 22:58:13 +0200

    Nu tilføjet mads' funktion forhåbentligt på rigtig vis =)

--- Patric_funk/patfunc.py
+++ Patric_funk/patfunc.py
@@ -1,3 +1,7 @@
+# publish_controller_data(0.1, 0.1,   0.1,   0,    0)
+#                          x,    y,   phi,  eat,  eject
+#publish_controller_data(0.1,0.1,0.1,0,0)
+
 # Example classes for objects
 class Car:
     def __init__(self, x, y, angle):
@@ -30,88 +34,58 @@ def move_to_target(target_position):
     
     # Move in the y direction
     if current_y != target_y:
-        if current_y < target_y:
-            #adjust_angle(car, 0)
-            if car.angle == 0:
-                adjust_angle(car, 0)
-            elif car.angle == 90:
-                adjust_angle(car, -90)
-            elif car.angle == 180:
-                adjust_angle(car, 180)
-            elif car.angle == 270:
-                adjust_angle(car, 90)
-            while current_y < target_y:
-                move_forward(car, 1)
-                current_y += 1
-        else:
-            adjust_angle(car, 180)
-            while current_y > target_y:
-                move_forward(car, 1)
-                current_y -= 1
+        if current_y < target_y: #vi skal altså op ad ^
+            #overvej at smid et threshold ind her
+            if car.angle > 0:
+                publish_controller_data(0,0,-0.1,0,0)
+                return
+            if car.angle < 0:
+                publish_controller_data(0,0,0.1,0,0)
+                return
+            publish_controller_data(0,0.1,0,0,0) #vi rykker 0.1 i y-retningen (opad)
+            return
+        else: #så skal vi altså nedad ˅
+            if car.angle != 180:
+                if car.angle > 0:
+                    publish_controller_data(0,0,0.1,0,0)
+                    return
+                if car.angle < 0:
+                    publish_controller_data(0,0,-0.1,0,0)
+                    return
+        publish_controller_data(0,0.1,0,0,0) #vi rykker 0.1 i y-retningen (opad)
+        return
     
     # Move in the x direction
     if current_x != target_x:
-        if current_x < target_x:
-            #adjust_angle(car, 90)
+        if current_x > target_x: #så skal vi til venstre
             if car.angle == 0:
-                adjust_angle(car, 90)
-            elif car.angle == 90:
-                adjust_angle(car, 0)
-            elif car.angle == 180:
-                adjust_angle(car, -90)
-            elif car.angle == 270:
-                adjust_angle(car, 180)
-            while current_x < target_x:
-                move_forward(car, 1)
-                current_x += 1
-        else:
+                publish_controller_data(0,0,-0.1,0,0)
+                return
+            if car.angle > 270:
+                publish_controller_data(0,0,-0.1,0,0)
+                return
+            if car.angle == 180:
+                publish_controller_data(0,0,0.1,0,0)
+                return    
+            if car.angle > 180:
+                publish_controller_data(0,0,0.1,0,0)
+                return
+            publish_controller_data(0,0.1,0,0,0)
+            return
+        else: #så skal vi til højre
             if car.angle == 0:
-                adjust_angle(car, -90)
-            elif car.angle == 90:
-                adjust_angle(car, 180)
-            elif car.angle == 180:
-                adjust_angle(car, 90)
-            elif car.angle == 270:
-                adjust_angle(car, 0)
-            while current_x > target_x:
-                move_forward(car, 1)
-                current_x -= 1
-    
-    # Update the car's position
-    car.x, car.y = current_x, current_y
-    print(f"New position of the car: ({car.x}, {car.y}) with angle {car.angle}")
-
-def move_forward(car, step):
-    print(f"Moving forward by {step} step(s) at angle {car.angle}")
-    # Logic to move forward considering current angle
-    if car.angle == 0:
-        car.y += step
-    elif car.angle == 180:
-        car.y -= step
-    elif car.angle == 90:
-        car.x += step
-    elif car.angle == 270:
-        car.x -= step
-
-def adjust_angle(car, new_angle):
-    if car.angle+new_angle < 0:
-        new_angle = 270
-    print(f"Adjusting angle from {car.angle} to {car.angle+new_angle} degrees")
-    car.angle += new_angle % 360
-    if car.angle == 360:
-        car.angle = 0
-
-
-# Example usage
-#my pos = 3,5   targ pos = 0,0 expected angle = 180+90 = 270
-target_position = (0, 0)
-move_to_target(target_position)
-#my pos = 0,0 targ pos = 3,5 expected angle = 0+90 = 90
-target_position = (3, 5)
-move_to_target(target_position)
-#my pos = 3,5 targ pos = 2,5 expected angle = 0+270 = 270 
-target_position = (2, 5)
-move_to_target(target_position)
-#my pos = 2,5 targ pos = 4,6 expected angle = 0+90 = 90
-target_position = (4, 6)
-move_to_target(target_position)
+                publish_controller_data(0,0,0.1,0,0)
+                return
+            if car.angle > 0:
+                publish_controller_data(0,0,0.1,0,0)
+                return
+            if car.angle == 180:
+                publish_controller_data(0,0,-0.1,0,0)
+                return    
+            if car.angle < 180:
+                publish_controller_data(0,0,-0.1,0,0)
+                return
+            publish_controller_data(0,0.1,0,0,0)
+            return
+    #nu er current_y = target_y, current_x = target_x
+    publish_controller_data(0,0,0,1,0) #og så skal der nedsvælges
