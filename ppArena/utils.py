bc4b2aa85597cefc9644eeb83aa83dbf2ada4196
s205057 <s205057@dtu.dk>
2024-06-08 15:40:05 +0200

    lil update

--- ppArena/utils.py
+++ ppArena/utils.py
@@ -125,10 +125,10 @@ def getImage():
     """This is just a dummy function. It will be replaced by the camera module."""
     
     # image = cv2.imread('test/images/WIN_20240403_10_40_59_Pro.jpg')
-    # image = cv2.imread('test/images/WIN_20240403_10_39_46_Pro.jpg') 
+    image = cv2.imread('test/images/WIN_20240403_10_39_46_Pro.jpg') 
     # image = cv2.imread('test/images/WIN_20240403_10_40_38_Pro.jpg') #hvid nej
     # image = cv2.imread('test/images/WIN_20240403_10_40_58_Pro.jpg') 
-    image = cv2.imread('test/images/pic50upsidedown.jpg') 
+    # image = cv2.imread('test/images/pic50upsidedown.jpg') 
     # image = cv2.imread('test/images/WIN_20240410_10_31_43_Pro.jpg') 
     # image = cv2.imread('test/images/WIN_20240410_10_31_07_Pro.jpg') 
     # image = cv2.imread('test/images/WIN_20240410_10_31_07_Pro.jpg') #orig pic with transfrom new
@@ -587,7 +587,7 @@ def detect_ball_colors(image):
     #regarding the arena
     for pic, contour in enumerate(contours): 
         area = cv2.contourArea(contour) 
-        if area > 300:
+        if area > 6000 and area < 8000: # area of cross is aprox 7000
             rect = cv2.minAreaRect(contour)
             box = cv2.boxPoints(rect)
             box = np.int0(box)
