d5c5f7781e71b69575fb485a6341c31f12e4e5e8
s205057 <s205057@dtu.dk>
2024-06-08 16:22:36 +0200

    save the balls update

--- ppArena/utils.py
+++ ppArena/utils.py
@@ -434,54 +434,23 @@ def circle_detection(image):
 
     # with open('stored_circles.json', 'w') as file:
     #     json.dump(stored_circles, file, indent=4)
-
+    save_balls(stored_circles)
+    hello = load_balls()
     # Display the result
     # cv2.imshow('Detected Balls', image)
     return image, stored_circles
 
-    # return image
 
-  ##
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
+def save_balls(circles, filename="balls.json"):
+    balls = [(int(circle['center'][0]), int(circle['center'][1])) for circle in circles]
+    with open(filename, 'w') as file:
+        json.dump(balls, file, indent=4)
 
-
-    ################################
-
-
-    # # Convert to grayscale
-    # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # cv2.imshow('gray', gray)
-    # # Blur using 3 * 3 kernel
-    # gray_blurred = cv2.blur(gray, (3, 3))
-    # cv2.imshow('gray blurred', gray_blurred)
-
-    # # Apply Hough transform on the blurred image
-    # detected_circles = cv2.HoughCircles(gray_blurred,
-    #                                     cv2.HOUGH_GRADIENT, 1, 20,
-    #                                     param1=50, param2=30,
-    #                                     minRadius=1, maxRadius=40)
-
-    # # Draw circles that are detected
-    # if detected_circles is not None:
-    #     # Convert the circle parameters a, b and r to integers
-    #     detected_circles = np.uint16(np.around(detected_circles))
-
-    #     for pt in detected_circles[0, :]:
-    #         a, b, r = pt[0], pt[1], pt[2]
-
-    #         # Draw the circumference of the circle
-    #         cv2.circle(image, (a, b), r, (0, 255, 0), 2)
-
-    #         # Draw a small circle (of radius 1) to show the center
-    #         cv2.circle(image, (a, b), 1, (0, 0, 255), 3)
-    #         cv2.imshow("Detected Circle", image)
-    #         cv2.waitKey(0)
-
-    # # Close the window when done
-    # cv2.destroyAllWindows()
-
-
+def load_balls(filename="balls.json"):
+    with open(filename, 'r') as file:
+        balls = json.load(file)
+    # Convert the list of lists back to a list of tuples
+    return [tuple(center) for center in balls]
 
 
 #TRUE
