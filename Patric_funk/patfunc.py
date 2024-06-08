cc8ff0776e4e6aab6e106c31f1af3a462f62922b
PatricTindo <patricseb@gmail.com>
2024-06-08 11:45:21 +0200

    aftenlogikken var dårlig, vi prøver lige igen..

--- Patric_funk/patfunc.py
+++ Patric_funk/patfunc.py
@@ -37,55 +37,59 @@ def move_to_target(target_position):
         if current_y < target_y: #vi skal altså op ad ^
             #overvej at smid et threshold ind her
             if car.angle > 0:
-                publish_controller_data(0,0,-0.1,0,0)
+                publish_controller_data(0,0,-0.1,0,0) #vi vender os mod venstre
                 return
-            if car.angle < 0:
-                publish_controller_data(0,0,0.1,0,0)
+            if car.angle > 270:
+                publish_controller_data(0,0,0.1,0,0) #vi vender os mod højre
                 return
             publish_controller_data(0,0.1,0,0,0) #vi rykker 0.1 i y-retningen (opad)
             return
         else: #så skal vi altså nedad ˅
             if car.angle != 180:
                 if car.angle > 0:
-                    publish_controller_data(0,0,0.1,0,0)
+                    publish_controller_data(0,0,0.1,0,0) #vi vender tilføjer grader indtil vi er på 180
                     return
-                if car.angle < 0:
-                    publish_controller_data(0,0,-0.1,0,0)
+                if car.angle > 180:
+                    publish_controller_data(0,0,-0.1,0,0)#vi "fjerner" grader indtil vi er på 180
                     return
         publish_controller_data(0,0.1,0,0,0) #vi rykker 0.1 i y-retningen (opad)
         return
     
     # Move in the x direction
+    # nu er vores angle enten 0 eller 180:
     if current_x != target_x:
         if current_x > target_x: #så skal vi til venstre
-            if car.angle == 0:
-                publish_controller_data(0,0,-0.1,0,0)
-                return
-            if car.angle > 270:
-                publish_controller_data(0,0,-0.1,0,0)
-                return
-            if car.angle == 180:
-                publish_controller_data(0,0,0.1,0,0)
-                return    
-            if car.angle > 180:
-                publish_controller_data(0,0,0.1,0,0)
-                return
+            if car.angle != 270:
+                if car.angle == 0:
+                    publish_controller_data(0,0,-0.1,0,0) #hvis vi peger opad ved 0 grader, skal vi bare tilte mod venstre
+                    return
+                if car.angle > 270: #efter at have trukket én fra 0, lander vi på 359 grader
+                    publish_controller_data(0,0,-0.1,0,0) #Hvis vi er større end 270, skal vi bare blive ved med at tilte mod venstre
+                    return
+                if car.angle == 180:
+                    publish_controller_data(0,0,0.1,0,0) #hvis vi allerede er ved 180, skal vi bare mod højre
+                    return    
+                if car.angle > 180:
+                    publish_controller_data(0,0,0.1,0,0) #Hvis vi er større end 180, skal vi fortsat mod højre
+                    return
             publish_controller_data(0,0.1,0,0,0)
             return
         else: #så skal vi til højre
-            if car.angle == 0:
-                publish_controller_data(0,0,0.1,0,0)
-                return
-            if car.angle > 0:
-                publish_controller_data(0,0,0.1,0,0)
-                return
-            if car.angle == 180:
-                publish_controller_data(0,0,-0.1,0,0)
-                return    
-            if car.angle < 180:
-                publish_controller_data(0,0,-0.1,0,0)
-                return
+            if car.angle != 90:
+                if car.angle == 180:
+                    publish_controller_data(0,0,-0.1,0,0)
+                    return    
+                if car.angle > 90:
+                    publish_controller_data(0,0,-0.1,0,0)
+                    return    
+                if car.angle == 0:
+                    publish_controller_data(0,0,0.1,0,0) #hvis vi peger opad ved 0 grader, skal vi bare tilte mod højre
+                    return
+                if car.angle > 0:
+                    publish_controller_data(0,0,0.1,0,0) #hvis vi er større end 0 skal vi også pege mod højre
+                    return
             publish_controller_data(0,0.1,0,0,0)
             return
     #nu er current_y = target_y, current_x = target_x
     publish_controller_data(0,0,0,1,0) #og så skal der nedsvælges
+
