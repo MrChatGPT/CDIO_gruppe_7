e796e0e3040fbce7392ff720736950d8972256ee
PatricTindo <patricseb@gmail.com>
2024-06-11 13:15:05 +0200

    added algo

--- run.py
+++ run.py
@@ -2,12 +2,17 @@ from matplotlib import pyplot as plt
 from picture.livefeed import *
 from picture.transform_arena import *
 from picture.image_detection import *
+#mangler at importere mads' funktioner
+
 
 # run image recognition software
 def transform_and_detect(image):
     image = transform(image)
     circle_detection(image) 
     image = detect_ball_colors(image)
+    car = find_car(image)
+    
+
 
 # initialize program
 def init():
@@ -28,6 +33,11 @@ try:
     while True:
         image = camera_handler._run_video()
         transform_and_detect(image)
+        #---- her forventes der at være 4 json filer: -------
+        # nogo_zones, ping_pong_balls, orange_ball og robot
+        # ---------------------------------------------------
+        # Herfra mangles: 
+        # algoritme som henter data fra .json filerne og så bevæger bilen
         
 finally:
     # Ensure the camera is released properly
