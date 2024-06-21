23cbb9970d617d06874bc981a107183b400f3dfc
madsr2d2 <mads.richardt@gmail.com>
2024-06-21 22:20:01 +0200

    mads

--- control_logic.py
+++ control_logic.py
@@ -34,7 +34,7 @@ class ControlLogic:
         distance_to_waypoint = data.get('distance_to_closest_waypoint')
         angle_errw = data.get('angle_to_closest_waypoint')
 
-        if waypoint is not None:
+        if waypoint is not None and distance_to_waypoint > 5:
             print(f"Moving to waypoint: {waypoint}")
             print(f"Distance to waypoint: {distance_to_waypoint}")
 
@@ -48,100 +48,19 @@ class ControlLogic:
             rotation = 0
             print(f"Control data: x={x}, y={y}, rotation={rotation}")
             self.controller.publish_control_data(x, y, rotation)
+        else:
+            print("No waypoint")
+            self.stop_robot()
 
-            # if angle_errw > 5:
-            #     self.controller.publish_control_data(0, 0, 0.11)
-            #     return
-            # elif angle_errw < -5:
-            #     self.controller.publish_control_data(0, 0, -0.11)
-            #     return
-            # if abs(angle_errw) < 5:
-            #     self.controller.publish_control_data(0, 0.12, 0)
-            #     return
-
-            return
-
-        #         self.controller.publish_control_data(x, y, rotation)
-        #     else:
-        #         self.stop_robot()
-
-        def stop_robot(self):
-            # Stop the robot if no waypoint
-            self.controller.publish_control_data(0, 0, 0)
-
-<<<<<<< HEAD
-        #----------------- Here we assume that the robot is on top of the waypoint ---------------------
-        #logic for ball: 
-        distance_to_ball = data.get('distance_to_closest_ball')
-        ball = data.get('vector_to_ball_robot_frame')
-        angle_err = data.get('angle_to_closest_ball')
-        if ball is not None:
-            print("Moving to ball: ", distance_to_ball)
-            speed = self.pid(-distance_to_ball)
-            print(f"Speed: {speed}")
-            #Angle_err {0..180} & {-180..-0}
-            if angle_err > 1:
-                self.controller.publish_control_data(0,0,0.11)
-                return
-            elif angle_err < -1:
-                self.controller.publish_control_data(0,0,-0.11)
-                return
-            if abs(angle_err) < 1:
-                self.controller.publish_control_data(0,0.12,0)
-                return
-            if (distance_to_ball) < 160:
-                self.controller.publish_control_data(0,0,0,1,0)
-                sleep(0.1)
-                self.controller.publish_control_data(0,0,0,0,0)
-                sleep(0.1)
-                self.controller.publish_control_data(0,0,0.12,0)
-                sleep(0.1)
-                self.controller.publish_control_data(0,0.15,0,0,0)
-                return
-
-
-            
-        
-        print("\n\n\nVi burde aldrig se det her print weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\n\n\n")
-            
-=======
-        # ----------------- Here we assume that the robot is on top of the waypoint ---------------------
-        # logic for ball:
-        # distance_to_ball = data.get('distance_to_closest_ball')
-        # ball = data.get('vector_to_ball_robot_frame')
-        # angle_err = data.get('angle_to_closest_ball')
-        # if ball is not None:
-        #     print("Moving to ball: ", distance_to_ball)
-        #     speed = self.pid(-distance_to_ball)
-        #     print(f"Speed: {speed}")
-        #     # Angle_err {0..180} & {-180..-0}
-        #     if angle_err > 1:
-        #         self.controller.publish_control_data(0, 0, 0.11)
-        #         return
-        #     elif angle_err < -1:
-        #         self.controller.publish_control_data(0, 0, -0.11)
-        #         return
-        #     if abs(angle_err) < 1:
-        #         self.controller.publish_control_data(0, 0.12, 0)
-        #         return
-        #     if (distance_to_ball) < 160:
-        #         self.controller.publish_control_data(0, 0, 0, 1, 0)
->>>>>>> 97556c0 (mads)
-
-        #         return
-
-        # print("\n\n\nVi burde aldrig se det her print weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\n\n\n")
+    def stop_robot(self):
+        # Stop the robot if no waypoint
+        self.controller.publish_control_data(0, 0, 0, 0, 0)
 
 
 if __name__ == "__main__":
     queue = Queue(maxsize=10)  # Set a reasonable max size for the queue
-<<<<<<< HEAD
-    # video_path = "/home/madsr2d2/sem4/CDIO/CDIO_gruppe_7/camera2/film_2.mp4"
-    video_path = 1
-=======
     video_path = "/home/madsr2d2/sem4/CDIO/CDIO_gruppe_7/camera2/film_2.mp4"
-    video_path = "/dev/video8"
->>>>>>> 97556c0 (mads)
+    video_path = "/dev/video9"
     # Initialize the Controller directly, which includes the MQTT client initialization
     broker_url = '192.168.1.101'
     broker_port = 1883
@@ -149,12 +68,16 @@ if __name__ == "__main__":
     controller = Controller(broker_url, broker_port, topic)
 
     # Initialize the PID controller
-    pid = PID(Kp=0.001, Ki=0.0, Kd=0.00, setpoint=0)
+
+    translation_pid = PID(Kp=0.001, Ki=0.0, Kd=0.0000, setpoint=0)
+    rotation_pid = PID(Kp=0.001, Ki=0.0, Kd=0.00, setpoint=0)
+
     # Limit the PID output to the range of 0 to 1
-    pid.output_limits = (0.15, 0.2)
+    translation_pid.output_limits = (0.15, 0.3)
+    rotation_pid.output_limits = (-0.2, 0.2)
 
     # Create an instance of the ControlLogic class
-    control_logic = ControlLogic(queue, controller, pid)
+    control_logic = ControlLogic(queue, controller, translation_pid)
 
     # Start the camera process
     camera_proc = Process(target=camera_process, args=(queue, video_path))
@@ -166,7 +89,9 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         camera_proc.terminate()
         camera_proc.join()
+        exit(0)
     finally:
         # Ensure the camera process is terminated when the control logic is done
         camera_proc.terminate()
         camera_proc.join()
+        exit(0)
