6d693846ebe51c4acb942d9a7b4b85248099c1d8
s203071 <s203071@student.dtu.dk>
2024-06-24 16:14:27 +0200

    Added a dumb waypoint check

--- control_logic.py
+++ control_logic.py
@@ -24,6 +24,7 @@ class ControlLogic:
         self.control_flags = control_flags
         self.ball_count = 0
         self.to_goal = False
+        self.arena_check = False
 
     def run(self):
         while True:
@@ -34,7 +35,6 @@ class ControlLogic:
                         self.collect_ball(data, color='white')
                     else:
                         self.score_ball(data)
-
                 else:
                     self.stop_robot()
             except Empty:
@@ -44,13 +44,13 @@ class ControlLogic:
                 break
 
     def collect_ball(self, data, color):
-
-        # get waypoint data
+    # Get the initial waypoint data
         vector_waypoint = data.get(f'vector_to_{color}_waypoint_robot_frame')
-        distance_to_waypoint = data.get(
-            f'distance_to_closest_{color}_waypoint')
+        distance_to_waypoint = data.get(f'distance_to_closest_{color}_waypoint')
         angle_err_to_waypoint = data.get(f'angle_to_closest_{color}_waypoint')
+        waypoints = data.get('arena_data')
 
+       
         # get ball data
         vector_ball = data.get(f'vector_to_{color}_ball_robot_frame')
         distance_to_ball = data.get(f'distance_to_closest_{color}_ball')
@@ -66,14 +66,142 @@ class ControlLogic:
         print(f"Angle error to {color}_ball: {angle_err_to_ball}")
         print('control flags:', self.control_flags)
 
-        # print(data)
+         # If the initial waypoint data is invalid, find the closest valid waypoint
+        if (len(vector_waypoint) == 0):
+            print("no balls reachable\n waypoint info = ", vector_waypoint)
+            self.arena_check = True
+            closest_waypoint = None
+            min_distance = float('inf')
+            
+            for waypoint in waypoints:
+                vector, distance, angle = waypoint
+                
+                if distance < min_distance:
+                    min_distance = distance
+                    closest_waypoint = waypoint
+            
+            if closest_waypoint:
+                vector_waypoint, distance_to_waypoint, angle_err_to_waypoint = closest_waypoint
 
-        try:
-            if not self.on_waypoint:
-                # reset control flags to false except for update_robot
+            # Check if the current waypoint matches any waypoint, and find the next closest one
+            current_waypoint = (vector_waypoint, distance_to_waypoint, angle_err_to_waypoint)
+            waypoints_sorted_by_distance = sorted(waypoints, key=lambda wp: wp[1])
+            
+            for wp in waypoints_sorted_by_distance:
+                if wp != current_waypoint:
+                    vector_waypoint, distance_to_waypoint, angle_err_to_waypoint = wp
+                    break
+
+        # print(data)
+        if(not self.arena_check):
+            try:
+                if not self.on_waypoint:
+                    # reset control flags to false except for update_robot
+                    for key in self.control_flags.keys():
+                        if key != 'update_robot':
+                            self.control_flags[key] = False
+
+                    if distance_to_waypoint > self.distance_tolerance:
+
+                        # Calculate the direction angle using the normalized orange_waypoint vector
+                        direction_angle = math.degrees(math.atan2(
+                            vector_waypoint[1], vector_waypoint[0]))
+
+                        # Determine if the robot is moving straight or diagonally
+                        if 45 <= abs(direction_angle) <= 135:
+                            speed_scale = 1  # Scale factor for diagonal movements
+                        else:
+                            speed_scale = 0.5  # Scale factor for straight movements
+
+                        # Calculate the speed using the distance to the waypoint
+                        speed = self.pid_translation(
+                            -distance_to_waypoint) * speed_scale
+                        y = vector_waypoint[0] * speed
+                        x = vector_waypoint[1] * speed
+
+                        # set rotation to 0
+                        rotation = 0
+                        self.controller.publish_control_data(x, y, rotation)
+
+                    else:
+                        self.stop_robot()
+                        print(f"Reached {color}_waypoint")
+
+                        time.sleep(1)
+                        self.on_waypoint = True
+
+                elif abs(angle_err_to_ball) > self.angle_tolerance:
+                    rotation = -self.pid_rotation(angle_err_to_ball)
+
+                    # set x and y to 0
+                    x = y = 0
+
+                    self.controller.publish_control_data(x, y, rotation)
+
+                elif distance_to_ball - robot_critical_length > self.distance_tolerance:
+                    # scale pid constants down for the last part of the movement by 0.5
+                    self.pid_translation.Kp = self.pid_translation.Kp * self.pid_scaling_factor
+                    self.pid_translation.Ki = self.pid_translation.Ki * self.pid_scaling_factor
+                    self.pid_translation.Kd = self.pid_translation.Kd * self.pid_scaling_factor
+
+                    # Calculate the direction angle using the normalized orange_ball vector
+                    direction_angle = math.degrees(math.atan2(
+                        vector_ball[1], vector_ball[0]))
+
+                    # Determine if the robot is moving straight or diagonally
+                    if 45 <= abs(direction_angle) <= 135:
+                        speed_scale = 1
+
+                    else:
+                        speed_scale = 0.5
+
+                    # Calculate the speed using the distance to the ball
+                    speed = self.pid_translation(
+                        -(distance_to_ball - robot_critical_length)) * speed_scale
+
+                    y = vector_ball[0] * speed
+
+                    x = vector_ball[1] * speed
+
+                    # set rotation to 0
+
+                    rotation = 0
+                    self.controller.publish_control_data(x, y, rotation)
+
+                else:
+                    # reset pid constants
+                    self.pid_translation.Kp = self.pid_translation.Kp / self.pid_scaling_factor
+                    self.pid_translation.Ki = self.pid_translation.Ki / self.pid_scaling_factor
+                    self.pid_translation.Kd = self.pid_translation.Kd / self.pid_scaling_factor
+                    # Collect the ball
+                    self.ball_count = self.ball_count+1
+                    # get data:
+                    white_balls = data.get(f'white_ball_centers')
+                    blocked_balls = data.get(f'blocked_white_centers')
+                    print(f"picking up {color} ball")
+                    self.ball_in()
+                    self.stop_robot()
+
+                    if (self.ball_count % 4 == 0) or ((len(white_balls) == 0) and (len(blocked_balls) == 0)):
+                        self.to_goal = True
+                    # set control flags to true
+                    for key in self.control_flags.keys():
+                        self.control_flags[key] = True
+
+                    print('Uddating arena, robot and balls')
+                    print('control flags:', self.control_flags)
+                    time.sleep(5)
+                    self.on_waypoint = False
+
+            except Exception as e:
+                print(f"Error occurred in collect_ball method: {e}")
+                print('Stopping robot')
+                self.stop_robot()
+        else:
+            try:
                 for key in self.control_flags.keys():
-                    if key != 'update_robot':
-                        self.control_flags[key] = False
+                        if key != 'update_robot':
+                            self.control_flags[key] = False
 
                 if distance_to_waypoint > self.distance_tolerance:
 
@@ -100,77 +228,15 @@ class ControlLogic:
                 else:
                     self.stop_robot()
                     print(f"Reached {color}_waypoint")
-
                     time.sleep(1)
-                    self.on_waypoint = True
+                    self.arena_check = False
 
-            elif abs(angle_err_to_ball) > self.angle_tolerance:
-                rotation = -self.pid_rotation(angle_err_to_ball)
-
-                # set x and y to 0
-                x = y = 0
-
-                self.controller.publish_control_data(x, y, rotation)
-
-            elif distance_to_ball - robot_critical_length > self.distance_tolerance:
-                # scale pid constants down for the last part of the movement by 0.5
-                self.pid_translation.Kp = self.pid_translation.Kp * self.pid_scaling_factor
-                self.pid_translation.Ki = self.pid_translation.Ki * self.pid_scaling_factor
-                self.pid_translation.Kd = self.pid_translation.Kd * self.pid_scaling_factor
-
-                # Calculate the direction angle using the normalized orange_ball vector
-                direction_angle = math.degrees(math.atan2(
-                    vector_ball[1], vector_ball[0]))
-
-                # Determine if the robot is moving straight or diagonally
-                if 45 <= abs(direction_angle) <= 135:
-                    speed_scale = 1
-
-                else:
-                    speed_scale = 0.5
-
-                # Calculate the speed using the distance to the ball
-                speed = self.pid_translation(
-                    -(distance_to_ball - robot_critical_length)) * speed_scale
-
-                y = vector_ball[0] * speed
-
-                x = vector_ball[1] * speed
-
-                # set rotation to 0
-
-                rotation = 0
-                self.controller.publish_control_data(x, y, rotation)
-
-            else:
-                # reset pid constants
-                self.pid_translation.Kp = self.pid_translation.Kp / self.pid_scaling_factor
-                self.pid_translation.Ki = self.pid_translation.Ki / self.pid_scaling_factor
-                self.pid_translation.Kd = self.pid_translation.Kd / self.pid_scaling_factor
-                # Collect the ball
-                self.ball_count = self.ball_count+1
-                # get data:
-                white_balls = data.get(f'white_ball_centers')
-                blocked_balls = data.get(f'blocked_white_centers')
-                print(f"picking up {color} ball")
-                self.ball_in()
+            except Exception as e:
+                print(f"Error occurred in arena_waypoint: {e}")
+                print('Stopping robot')
                 self.stop_robot()
 
-                if (self.ball_count % 4 == 0) or ((len(white_balls) == 0) and (len(blocked_balls) == 0)):
-                    self.to_goal = True
-                # set control flags to true
-                for key in self.control_flags.keys():
-                    self.control_flags[key] = True
-
-                print('Uddating arena, robot and balls')
-                print('control flags:', self.control_flags)
-                time.sleep(5)
-                self.on_waypoint = False
-
-        except Exception as e:
-            print(f"Error occurred in collect_ball method: {e}")
-            print('Stopping robot')
-            self.stop_robot()
+            
 
     def stop_robot(self):
         self.controller.publish_control_data(0, 0, 0)
